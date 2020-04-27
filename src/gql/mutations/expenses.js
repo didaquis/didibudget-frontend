6e6322c6b4acf53fe9aed5f086493b4f397b29e1
Dídac García <didaquis@gmail.com>
2020-04-27 19:31:47 +0200

    Added query and mutation for expenses

---
+++ src/gql/mutations/expenses.js
@@ -0,0 +1,26 @@
+import gql from 'graphql-tag';
+
+
+export const REGISTER_EXPENSE = gql`
+mutation registerExpense($category: ID!,$subcategory: ID, $quantity: Float!, $date: String!) {
+	registerExpense(category: $category, subcategory: $subcategory, quantity: $quantity, date: $date) {
+		quantity
+		date
+		currencyISO
+		uuid
+	}
+}
+`
+
+export const DELETE_EXPENSE = gql`
+mutation deleteExpense($uuid: String!) {
+	deleteExpense(uuid: $uuid) {
+    category
+    subcategory
+		quantity
+		date
+		currencyISO
+		uuid
+	}
+}
+`;
---
+++ src/gql/queries/expenses.js
@@ -0,0 +1,14 @@
+import gql from 'graphql-tag'
+
+export const LIST_ALL_EXPENSES = gql`
+query getExpenses {
+	getExpenses {
+		category
+    subcategory
+    quantity
+		date
+		currencyISO
+		uuid
+	}
+}
+`;
