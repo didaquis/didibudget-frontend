c09bf390b0064641ae440001e302c299942cd061
Dídac García <didaquis@gmail.com>
2020-05-08 17:42:38 +0200

    Remove unnecessary comment

--- src/components/Footer/index.test.js
+++ src/components/Footer/index.test.js
@@ -1,13 +1,3 @@
-
-// 		<footer className="footer mt-auto py-3 fixed-bottom bg-dark border-top border-info">
-// 			<div className="container text-center">
-// 				<span className="text-muted font-weight-light">
-// 					Made by <a className="text-info" target="_blank" rel="noreferrer noopener" href="https://didaquis.github.io/">didaquis</a>
-// 				</span>
-// 			</div>
-// 		</footer>
-
-
 import React from 'react'
 import { render, cleanup, screen } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
