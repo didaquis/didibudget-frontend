f6ed5fbcf8dfbd1f92e0f989328ed2da104e5d33
Dídac García <didaquis@gmail.com>
2020-05-08 16:13:38 +0200

    Added more test

---
+++ src/components/ErrorAlert/index.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import { ErrorAlert } from './'
+
+it('renders correctly', () => {
+	const { getByText } = render(<ErrorAlert errorMessage='foo' />)
+
+	expect(getByText('foo')).toBeInTheDocument()
+})
