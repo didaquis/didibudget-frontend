dc0302f6443a19a7a36ccf286b044addbf34452d
Dídac García <didaquis@gmail.com>
2020-05-09 00:19:47 +0200

    Added test

---
+++ src/hooks/useInputValue.test.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import { act } from 'react-dom/test-utils';
+
+import { useInputValue } from './useInputValue'
+
+describe('useInputValue', () => {
+	afterEach(cleanup)
+
+	test('should return two properties', () => {
+		let input
+		const Component = () => {
+			input = useInputValue(42)
+			return null;
+		}
+		render(<Component />)
+
+		expect(input.value).toBeDefined()
+		expect(typeof input.value).toBe('number')
+
+		expect(input.onChange).toBeDefined()
+		expect(typeof input.onChange).toBe('function')
+		expect(input.onChange).toBeInstanceOf(Function)
+	})
+
+	test('should set a string value', () => {
+		let input
+		const Component = () => {
+			input = useInputValue('default')
+			return null;
+		}
+		render(<Component />)
+
+		expect(input.value).toBe('default')
+		expect(typeof input.value).toBe('string')
+	})q
+
+	test('should change value', () => {
+		let input
+		const Component = () => {
+			input = useInputValue('')
+			return null;
+		}
+		render(<Component />)
+
+		act(() => {
+			const changedValue = { target: { value: 'foo' } }
+			input.onChange(changedValue)
+		})
+
+		expect(input.value).toBe('foo')
+	})
+})
