8286c618e4c7a28b6c52d2c51eeb05d9dfe10332
Dídac García <didaquis@gmail.com>
2020-07-28 00:00:46 +0200

    Added user information in Home page

---
+++ src/components/UserCard/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+export const UserCard = ( { userData } ) => (
+	<section className="mt-4">
+		<div className="card">
+			<div className="card-header">
+				<h4 className="mb-0">
+					Your user data
+				</h4>
+			</div>
+			<div className="card-body pb-0">
+				<p>You are logged as: <span className="font-weight-light pl-3">{userData.email}</span></p>
+				{
+					userData.isAdmin && <p>You are an administration user!</p>
+				}
+			</div>
+		</div>
+	</section>
+)
+
+UserCard.propTypes = {
+	userData: PropTypes.shape({
+		email: PropTypes.string.isRequired,
+		isAdmin: PropTypes.bool.isRequired,
+	})
+}
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { Fragment, useContext } from 'react'
+import { AuthContext } from '../AuthContext'
 
 import { Jumbotron } from '../components/Jumbotron'
+import { UserCard } from '../components/UserCard'
 
 const Home = () => {
+	const { userData } = useContext(AuthContext)
+
 	return (
-		<Jumbotron title='Home' subtitle='Welcome! This is an app to manage your money' />
+		<Fragment>
+			<Jumbotron title='Home' subtitle='Welcome! This is an app to manage your money' />
+			{
+				Object.keys(userData).length && <UserCard userData={userData} />
+			}
+		</Fragment>
 	)
 }
 
