e886453358fcbca0d6801e85a0555cda9cb3007c
Dídac García <didaquis@gmail.com>
2020-12-03 00:04:05 +0100

    Fix a typo

--- src/components/GetListOfUsers.js
+++ src/components/GetListOfUsers.js
@@ -9,8 +9,8 @@ import { LIST_ALL_USERS } from '../gql/queries/users'
 
 export const GetListOfUsers = () => {
 	const minuteInMilliseconds = 60000
-	const tenMinute = minuteInMilliseconds * 10
-	const { loading, error, data } = useQuery(LIST_ALL_USERS, { fetchPolicy: 'no-cache', pollInterval: tenMinute });
+	const tenMinutes = minuteInMilliseconds * 10
+	const { loading, error, data } = useQuery(LIST_ALL_USERS, { fetchPolicy: 'no-cache', pollInterval: tenMinutes });
 
 	if (loading) return <Spinner />
 	if (error) return <ErrorAlert errorMessage={error.message} />
