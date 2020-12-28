ec69a351e10c0f353c3c064493497af6dae10042
Dídac García <didaquis@gmail.com>
2020-12-28 23:28:29 +0100

    Added more component test

---
+++ src/components/UserCard/index.test.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { render, cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+
+import { UserCard } from './'
+
+describe('UserCard', () => {
+	afterEach(cleanup)
+
+	it('renders correctly and show the email', () => {
+		const data = {
+			email: 'foo@mail.com',
+			isAdmin: false
+		}
+		const { getByText } = render(<UserCard userData={data} />)
+
+		expect(getByText('foo@mail.com')).toBeInTheDocument()
+	})
+
+	it('renders correctly and show if you are an administrator user', () => {
+		const props = {
+			email: 'foo@mail.com',
+			isAdmin: false
+		}
+		const { queryByText, rerender } = render(<UserCard userData={props} />)
+
+		expect(queryByText('You are an administrator user!')).not.toBeInTheDocument()
+
+		const newProps = {
+			email: 'foo@mail.com',
+			isAdmin: true
+		}
+		rerender(<UserCard userData={newProps} />)
+		expect(queryByText('You are an administrator user!')).toBeInTheDocument()
+		
+	})
+})
