dc2ceb2eb5120076d722dd965d0c201691baaefc
Dídac García <didaquis@gmail.com>
2021-01-02 14:24:24 +0100

    Added logo main page

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -1,10 +1,15 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import { DidibudgetLogo } from '../DidibudgetLogo'
+
 export const Jumbotron = ({ title, subtitle }) => (
 	<div className="jumbotron">
-		<h2 data-easteregg="The cake is a lie" className="display-4 mb-4">{title}</h2>
-		<p className="lead">{subtitle}</p>
+		<div className="col-sm-12 offset-md-2 col-md-8 col-lg-8 mb-5">
+			<DidibudgetLogo />
+		</div>
+		<h2 className="display-4 m-4" data-easteregg="The cake is a lie">{title}</h2>
+		<p className="m-4 lead">{subtitle}</p>
 	</div>
 )
 
--- src/components/Jumbotron/index.test.js
+++ src/components/Jumbotron/index.test.js
@@ -5,27 +5,11 @@ import '@testing-library/jest-dom/extend-expect'
 import { Jumbotron } from './'
 
 describe('Jumbotron', () => {
-	it('renders correctly', () => {
+	it('contains an expected text', () => {
 		const { getByText } = render(<Jumbotron title='foo' subtitle='bar' />)
 
 		expect(getByText('foo')).toBeInTheDocument()
 		expect(getByText('bar')).toBeInTheDocument()
-	})
-
-	it('contains an expected text', () => {
-		render(<Jumbotron title='foo' subtitle='bar' />)
-
-		const expectedText = 'foobar'
-
-		screen.getByText((content, node) => {
-			const hasText = (node) => node.textContent === expectedText
-			const nodeHasText = hasText(node)
-			const childrenDontHaveText = Array.from(node.children).every(
-			  (child) => !hasText(child)
-			)
-
-			return nodeHasText && childrenDontHaveText
-		})
 	})
 
 	it('contains the correct DOM nodes', () => {
@@ -37,13 +21,13 @@ describe('Jumbotron', () => {
 
 		expect(nodeTitle.localName).toBe('h2')
 		expect(nodeTitle.dataset.easteregg).toBe('The cake is a lie')
-		expect(nodeTitle.className).toBe('display-4 mb-4')
+		expect(nodeTitle.className).toBe('display-4 m-4')
 
 
 		const nodeSubtitle = screen.getByText('my-subtitle')
 
 		expect(nodeSubtitle.localName).toBe('p')
-		expect(nodeSubtitle.className).toBe('lead')
+		expect(nodeSubtitle.className).toBe('m-4 lead')
 
 		/*
 			Other interesting properties of nodes:
