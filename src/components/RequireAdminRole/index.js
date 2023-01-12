4a38cc03dfb0f17162ba0b194ac7a79bd95bd753
Dídac García <didaquis@gmail.com>
2023-01-12 23:42:03 +0100

    Remove unnecessary chars

--- src/components/RequireAdminRole/index.js
+++ src/components/RequireAdminRole/index.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { Navigate } from 'react-router-dom' 
+import { Navigate } from 'react-router-dom'
 
 import { AuthContext } from '../../AuthContext'
 
--- src/components/RequireAuth/index.js
+++ src/components/RequireAuth/index.js
@@ -1,5 +1,5 @@
 import { useContext } from 'react'
-import { Navigate, useLocation } from 'react-router-dom' 
+import { Navigate, useLocation } from 'react-router-dom'
 
 import { AuthContext } from '../../AuthContext'
 
