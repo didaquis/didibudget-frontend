bc716a9108c6c0f2446031278e0f3693b9dce07a
Dídac García <didaquis@gmail.com>
2023-02-23 00:15:37 +0100

    Add graphql query

--- src/components/MonthlyBalance/GetListOfMonthlyBalance.js
+++ src/components/MonthlyBalance/GetListOfMonthlyBalance.js
@@ -4,10 +4,10 @@ import { Spinner } from '../Spinner'
 import { ErrorAlert } from '../ErrorAlert'
 import { ListOfMonthlyBalance } from './ListOfMonthlyBalance'
 
-import { LIST_ALL_MONTHLY_BALANCE } from '../../gql/queries/monthlyBalance'
+import { LIST_ALL_MONTHLY_BALANCES } from '../../gql/queries/monthlyBalances'
 
 export const GetListOfMonthlyBalance = () => {
-	const { loading, error, data, refetch } = useQuery(LIST_ALL_MONTHLY_BALANCE, { fetchPolicy: 'no-cache' })
+	const { loading, error, data, refetch } = useQuery(LIST_ALL_MONTHLY_BALANCES, { fetchPolicy: 'no-cache' })
 
 	if (loading) return <Spinner />
 	if (error) return <ErrorAlert errorMessage={error.message} />
