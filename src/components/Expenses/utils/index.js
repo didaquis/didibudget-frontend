dce8c4ed51e091c0ab3ef14b1a8b7801ea86164e
Dídac García <didaquis@gmail.com>
2024-03-23 15:54:08 +0100

    Fix docs

--- src/components/Expenses/utils/index.js
+++ src/components/Expenses/utils/index.js
@@ -370,8 +370,8 @@ const getDetailedExpensesPerMonth = (rawData = []) => {
  * @param {string} expensesInThisGroup.date - A valid date with this format: '1514447205699'
  * @param {string} expensesInThisGroup.currencyISO - A currency. Example: 'EUR'
  * @param {string} expensesInThisGroup.uuid - An UUID value
- * @param {Date} startDate - The initial date of the range
- * @param {Date} endDate - The end date of the range
+ * @param {string} startDate - The initial date of the range
+ * @param {string} endDate - The end date of the range
  * @returns {Object | null}
  */
 const getDetailedExpensesGroupedFromRange = (expensesInThisGroup = [], startDate = null, endDate = null) => {
