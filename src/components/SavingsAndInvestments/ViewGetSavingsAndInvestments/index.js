08a0138102e45e4dd4cb31e33ae571bde9b49ed3
Dídac García <didaquis@gmail.com>
2025-10-08 22:35:09 +0200

    Display data

--- src/components/SavingsAndInvestments/ViewGetSavingsAndInvestments/index.js
+++ src/components/SavingsAndInvestments/ViewGetSavingsAndInvestments/index.js
@@ -1,61 +1,41 @@
 import { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
-
-import { PageSubTitle } from '../../PageSubTitle'
-
-
-export const ViewGetSavingsAndInvestments = ({data}) => {	
+export const ViewGetSavingsAndInvestments = ({ data }) => {
 	return (
 		<Fragment>
-			<PageSubTitle text="Average spending: (excluding the current month)"/>
-			<p>se muestra el componente presentacional</p>
+			<section className="table-responsive">
+				<table className="table table-dark table-hover">
+					<thead>
+						<tr className="table-info text-dark">
+							<th scope="col">Concept</th>
+							<th scope="col">Total invested</th>
+						</tr>
+					</thead>
+					<tbody>
+						{
+							data.map(expenseSum => {
+								return (
+									<tr key={expenseSum.categoryType}>
+										<td>{expenseSum.categoryType}</td>
+										<td>{expenseSum.sum} {expenseSum.currencyISO}</td>
+									</tr>
+								)
+							})
+						}
+					</tbody>
+				</table>
+			</section>
 		</Fragment>
 	)
 }
 
 ViewGetSavingsAndInvestments.propTypes = {
-	data: PropTypes.array.isRequired,
-}
-/**
-tartDate: PropTypes.object.isRequired,
-	endDate: PropTypes.object.isRequired,
-	expenses: PropTypes.arrayOf(
+	data: PropTypes.arrayOf(
 		PropTypes.shape({
-			date: PropTypes.string.isRequired,
-			uuid: PropTypes.string.isRequired,
-			category: PropTypes.string.isRequired,
-			subcategory: PropTypes.string,
-			quantity: PropTypes.number.isRequired,
+			categoryType: PropTypes.string.isRequired,
+			sum: PropTypes.number.isRequired,
 			currencyISO: PropTypes.string.isRequired
 		})
-	), 
-
-
-AnalysisOfExpenses.propTypes = {
-	expenses: PropTypes.arrayOf(
-		PropTypes.shape({
-			date: PropTypes.string.isRequired,
-			uuid: PropTypes.string.isRequired,
-			category: PropTypes.string.isRequired,
-			subcategory: PropTypes.string,
-			quantity: PropTypes.number.isRequired,
-			currencyISO: PropTypes.string.isRequired
-		})
-	),
-	categories: PropTypes.arrayOf(
-		PropTypes.shape({
-			_id: PropTypes.string.isRequired,
-			name: PropTypes.string.isRequired,
-			subcategories: PropTypes.arrayOf(
-				PropTypes.shape({
-					_id: PropTypes.string.isRequired,
-					name: PropTypes.string.isRequired,
-					uuid: PropTypes.string.isRequired
-				})
-			),
-			uuid: PropTypes.string.isRequired
-		})
-	)
+	).isRequired,
 }
- */
