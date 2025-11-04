780573536c5e1460ef7876b29447f61b93f8c730
Dídac García <didaquis@gmail.com>
2025-11-04 23:52:44 +0100

    Add query to register new expense

--- src/components/Expenses/RecurringExpenseSuggestion/index.js
+++ src/components/Expenses/RecurringExpenseSuggestion/index.js
@@ -1,13 +1,39 @@
+import { useState } from 'react'
+import { useMutation } from '@apollo/client'
+import { useNavigate } from 'react-router-dom'
 import PropTypes from 'prop-types'
 
 import { SubmitButton } from '../../SubmitButton'
 import { EmojiListFromCategoryOrSubcategory } from '../../EmojiListFromCategoryOrSubcategory'
 
-export const RecurringExpenseSuggestion = ({ suggestion }) => {
+import { REGISTER_EXPENSE } from '../../../gql/mutations/expenses'
 
-	const onSubmit = (event) => {
-		// TODO: pending to implement
-		console.log('submit')
+export const RecurringExpenseSuggestion = ({ suggestion }) => {
+	const navigate = useNavigate()
+	const [isDisabled, setIsDisabled] = useState(false)
+
+	const [ registerExpense ] = useMutation(REGISTER_EXPENSE)
+
+	const onSubmit = async (event) => {
+		try {
+			event.preventDefault()
+			setIsDisabled(true)
+
+			const variables = {
+				category: suggestion.suggestedExpense.category,
+				subcategory: suggestion.suggestedExpense.subcategory,
+				quantity: suggestion.suggestedExpense.quantity,
+				date: new Date()
+			}
+
+			await registerExpense({ variables })
+
+			navigate('/expenses-administration')
+		} catch (error) {
+			console.error('Error registering expense:', error)
+		} finally {
+			setIsDisabled(false)
+		}
 	}
 
 	const emojis = [...new Set([...suggestion.suggestedExpense.categoryEmojis, ...suggestion.suggestedExpense.subcategoryEmojis])]
@@ -17,8 +43,8 @@ export const RecurringExpenseSuggestion = ({ suggestion }) => {
 			<div className="card bg-dark border-info text-light mb-3">
 				<div className="card-body">
 					<h5 className="card-title">{suggestion.suggestedExpense.categoryName} {(suggestion.suggestedExpense.subcategoryName) ? ` - ${suggestion.suggestedExpense.subcategoryName}` : ''} <EmojiListFromCategoryOrSubcategory emojis={emojis} /></h5>
-					<p className="card-text"><span class="text-nowrap">{suggestion.suggestedExpense.quantity} EUR</span></p>
-					<SubmitButton disabled={false} onClick={onSubmit}>Save expense</SubmitButton>
+					<p className="card-text"><span className="text-nowrap">{suggestion.suggestedExpense.quantity} EUR</span></p>
+					<SubmitButton disabled={isDisabled} onClick={onSubmit}>Save expense</SubmitButton>
 				</div>
 			</div>
 		</div>
