4e0a5563fd331d993a67a89799c265d2d97946d6
Dídac García <didaquis@gmail.com>
2025-12-05 19:40:12 +0100

    Get average per month

--- src/components/Expenses/AveragePerMonth/index.js
+++ src/components/Expenses/AveragePerMonth/index.js
@@ -1,47 +1,91 @@
-import { Fragment } from 'react'
+import { Fragment, useContext } from 'react'
 import PropTypes from 'prop-types'
-import { getAveragePerMonth, averageOfLast } from '../utils'
 
+import { getAveragePerMonth, averageOfLast, monthsBetweenDates } from '../utils'
+import { AuthContext } from '../../../AuthContext'
 import { PageSubTitle } from '../../PageSubTitle'
 
-export const AveragePerMonth = ({ data }) => {
-	const averagePerMonthExceptSecondLast = getAveragePerMonth(data)
+export const AveragePerMonth = ({ averageData, averageDataExcludingSavings }) => {
 
-	const lastThreeMonths = averageOfLast(averagePerMonthExceptSecondLast, 3)
-	const lastSixMonths = averageOfLast(averagePerMonthExceptSecondLast, 6)
-	const lastTwelveMonths = averageOfLast(averagePerMonthExceptSecondLast, 12)
-	const lastTwentyFourMonths = averageOfLast(averagePerMonthExceptSecondLast, 24)
+	const {userData } = useContext(AuthContext)
 
-	if (!!lastThreeMonths || !!lastSixMonths || !!lastTwelveMonths || !!lastTwentyFourMonths) {
-		return (
-			<Fragment>
-				<PageSubTitle text="Average spending:"/>
-			{
-				!!lastThreeMonths && <p className="font-weight-light text-light">
-					Over past <strong>3 months</strong>, the average monthly spending has been <span className="text-nowrap">{lastThreeMonths} €.</span>
-				</p>
-			}
-			{
-				!!lastSixMonths && <p className="font-weight-light text-light">
-					Over past <strong>6 months</strong>, the average monthly spending has been <span className="text-nowrap">{lastSixMonths} €.</span>
-				</p>
-			}
-			{
-				!!lastTwelveMonths && <p className="font-weight-light text-light">
-					Over past <strong>12 months</strong>, the average monthly spending has been <span className="text-nowrap">{lastTwelveMonths} €.</span>
-				</p>
-			}
-			{
-				!!lastTwentyFourMonths && <p className="font-weight-light text-light">
-					Over past <strong>24 months</strong>, the average monthly spending has been <span className="text-nowrap">{lastTwentyFourMonths} €.</span>
-				</p>
-			}
-			</Fragment>
-		)
-	}
+	const monthsSinceUserJoined = monthsBetweenDates(new Date(userData.registrationDate), new Date())
+
+	console.log(monthsSinceUserJoined)
+
+	return (		<Fragment></Fragment>)
+	// const averagePerMonthExceptSecondLast = getAveragePerMonth(data)
+
+	// const lastThreeMonths = averageOfLast(averagePerMonthExceptSecondLast, 3)
+	// const lastSixMonths = averageOfLast(averagePerMonthExceptSecondLast, 6)
+	// const lastTwelveMonths = averageOfLast(averagePerMonthExceptSecondLast, 12)
+	// const lastTwentyFourMonths = averageOfLast(averagePerMonthExceptSecondLast, 24)
+
+	// if (!!lastThreeMonths || !!lastSixMonths || !!lastTwelveMonths || !!lastTwentyFourMonths) {
+	// 	return (
+	// 		<Fragment>
+	// 			<PageSubTitle text="Average spending:"/>
+	// 		{
+	// 			!!lastThreeMonths && <p className="font-weight-light text-light">
+	// 				Over past <strong>3 months</strong>, the average monthly spending has been <span className="text-nowrap">{lastThreeMonths} €.</span>
+	// 			</p>
+	// 		}
+	// 		{
+	// 			!!lastSixMonths && <p className="font-weight-light text-light">
+	// 				Over past <strong>6 months</strong>, the average monthly spending has been <span className="text-nowrap">{lastSixMonths} €.</span>
+	// 			</p>
+	// 		}
+	// 		{
+	// 			!!lastTwelveMonths && <p className="font-weight-light text-light">
+	// 				Over past <strong>12 months</strong>, the average monthly spending has been <span className="text-nowrap">{lastTwelveMonths} €.</span>
+	// 			</p>
+	// 		}
+	// 		{
+	// 			!!lastTwentyFourMonths && <p className="font-weight-light text-light">
+	// 				Over past <strong>24 months</strong>, the average monthly spending has been <span className="text-nowrap">{lastTwentyFourMonths} €.</span>
+	// 			</p>
+	// 		}
+	// 		</Fragment>
+	// 	)
+	// }
 
 }
 
 AveragePerMonth.propTypes = {
-	data: PropTypes.array.isRequired,
+	averageData: PropTypes.shape({
+		lastThreeMonthsAverage: PropTypes.shape({
+			average: PropTypes.number.isRequired,
+			currencyISO: PropTypes.string.isRequired,
+		}).isRequired,
+		lastSixMonthsAverage: PropTypes.shape({
+			average: PropTypes.number.isRequired,
+			currencyISO: PropTypes.string.isRequired,
+		}).isRequired,
+		lastTwelveMonthsAverage: PropTypes.shape({
+			average: PropTypes.number.isRequired,
+			currencyISO: PropTypes.string.isRequired,
+		}).isRequired,
+		lastTwentyFourMonthsAverage: PropTypes.shape({
+			average: PropTypes.number.isRequired,
+			currencyISO: PropTypes.string.isRequired,
+		}).isRequired,
+	}).isRequired,
+	averageDataExcludingSavings: PropTypes.shape({
+		lastThreeMonthsAverage: PropTypes.shape({
+			average: PropTypes.number.isRequired,
+			currencyISO: PropTypes.string.isRequired,
+		}).isRequired,
+		lastSixMonthsAverage: PropTypes.shape({
+			average: PropTypes.number.isRequired,
+			currencyISO: PropTypes.string.isRequired,
+		}).isRequired,
+		lastTwelveMonthsAverage: PropTypes.shape({
+			average: PropTypes.number.isRequired,
+			currencyISO: PropTypes.string.isRequired,
+		}).isRequired,
+		lastTwentyFourMonthsAverage: PropTypes.shape({
+			average: PropTypes.number.isRequired,
+			currencyISO: PropTypes.string.isRequired,
+		}).isRequired,
+	}).isRequired,
 }
--- src/components/Expenses/GetDataExpenses.js
+++ src/components/Expenses/GetDataExpenses.js
@@ -3,14 +3,20 @@ import { useQuery } from '@apollo/client'
 import { Spinner } from '../Spinner'
 import { ErrorAlert } from '../ErrorAlert'
 import { GraphExpensesData } from './GraphExpensesData'
+import { CategoryType } from '../SavingsAndInvestments/utils'
 
-import { LIST_ALL_EXPENSES } from '../../gql/queries/expenses'
+import { LIST_ALL_EXPENSES, GET_EXPENSES_AVERAGES } from '../../gql/queries/expenses'
 
 export const GetDataExpenses = () => {
-	const { loading, error, data } = useQuery(LIST_ALL_EXPENSES, { fetchPolicy: 'no-cache' })
+	const listAllExpensesQuery = useQuery(LIST_ALL_EXPENSES, { fetchPolicy: 'no-cache' })
+	const expensesAverageQuery = useQuery(GET_EXPENSES_AVERAGES, { variables: { excludedCategoryTypes: [] } }, { fetchPolicy: 'no-cache' })
+	const expensesAverageExcludingSavingsQuery = useQuery(GET_EXPENSES_AVERAGES, { variables: { excludedCategoryTypes: [CategoryType.INVESTMENT, CategoryType.PENSION_PLAN] } }, { fetchPolicy: 'no-cache' })
 
-	if (loading) return <Spinner />
-	if (error) return <ErrorAlert errorMessage={error.message} />
 
-	return <GraphExpensesData data={data.getExpenses} />
+	if (listAllExpensesQuery.loading || expensesAverageQuery.loading || expensesAverageExcludingSavingsQuery.loading) return <Spinner />
+	if (listAllExpensesQuery.error) return <ErrorAlert errorMessage={listAllExpensesQuery.error.message} />
+	if (expensesAverageQuery.error) return <ErrorAlert errorMessage={expensesAverageQuery.error.message} />
+	if (expensesAverageExcludingSavingsQuery.error) return <ErrorAlert errorMessage={expensesAverageExcludingSavingsQuery.error.message} />
+
+	return <GraphExpensesData graphData={listAllExpensesQuery.data.getExpenses} averageData={expensesAverageQuery.data} averageDataExcludingSavings={expensesAverageExcludingSavingsQuery.data} />
 }
