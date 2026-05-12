41984374f61b6b35d399b65cf54efa1ddc5aea01
Dídac García <didaquis@gmail.com>
2026-05-12 17:01:35 +0200

    Code refactor

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 
