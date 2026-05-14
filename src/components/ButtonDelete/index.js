84b982299fb68da938975b1f2b6c2da9f74efaef
Dídac García <didaquis@gmail.com>
2026-05-14 22:08:40 +0200

    Improve UI

--- src/components/ButtonDelete/index.js
+++ src/components/ButtonDelete/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap'
 
-export const ButtonDelete = ( { uuid, deleteMutation, onDelete } ) => {
+export const ButtonDelete = ({ uuid, deleteMutation, onDelete }) => {
 	const [modal, setModal] = useState(false)
 	const toggle = () => setModal(!modal)
 
@@ -34,7 +34,7 @@ export const ButtonDelete = ( { uuid, deleteMutation, onDelete } ) => {
 					Are you sure you want to delete this registry?
 				</ModalBody>
 				<ModalFooter>
-					<Button color="info" outline={true} onClick={toggle}>Cancel</Button>
+					<Button outline={true} onClick={toggle}>Cancel</Button>
 					<Button color="danger" outline={true} onClick={handleClick} disabled={isDisabled}>Delete</Button>
 				</ModalFooter>
 				{
