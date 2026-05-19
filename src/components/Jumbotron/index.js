660b9375e2371d2aeed2536c12998d321eec7d5c
Dídac García <didaquis@gmail.com>
2026-05-19 20:25:54 +0200

    Improve UI

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types'
 import { DidibudgetLogo } from '../DidibudgetLogo'
 
 export const Jumbotron = ({ title, subtitle, subtitleExtraLine }) => (
-	<div className="bg-light p-4 rounded-3">
-		<div className="col-sm-12 offset-md-2 col-md-8 col-lg-8">
+	<div className="bg-light p-2 rounded-3">
+		<div className="col-sm-12 offset-md-2 col-md-8 col-lg-6 offset-lg-3">
 			<DidibudgetLogo />
 		</div>
 		<h2 className="fs-1 fw-light m-4" data-easteregg="The cake is a lie">{title}</h2>
