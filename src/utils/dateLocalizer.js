d804b78ab91af1ad36ea1edd1c5f33ccacf12ebe
Dídac García <didaquis@gmail.com>
2026-07-30 01:55:41 +0200

    Minor fix on Moment config

--- src/utils/dateLocalizer.js
+++ src/utils/dateLocalizer.js
@@ -5,7 +5,7 @@ import momentLocalizer from 'react-widgets-moment'
  * Importing this module configures the react-widgets date localizer. It has no exports: import it
  * for the side effect, once, from any component rendering a react-widgets date component.
  */
-Moment.locale('en', {
+Moment.updateLocale('en', {
 	week: {
 		dow: 1 // Monday is the first day of the week.
 	}
