f376608fca0cdc253fe4b795c02cacadd0d74356
Dídac García <didaquis@gmail.com>
2026-07-31 11:32:12 +0200

    fix: use the app's info accent for the 404 button

--- src/pages/Page404.js
+++ src/pages/Page404.js
@@ -7,6 +7,6 @@ export const Page404 = () => (
 	<Fragment>
 		<PageTitle text='Page not found' />
 		<p className="text-light">The page you are looking for does not exist or has been moved.</p>
-		<Link to='/' className="btn btn-primary">Go to the home page</Link>
+		<Link to='/' className="btn btn-outline-info">Go to the home page</Link>
 	</Fragment>
 )
